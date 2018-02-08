df47dfbd6f4aad54dc1285f24156336d53548d23
zhangxu <1010098203@qq.com>
2018-02-08 16:37:35 +0800

    init

---
+++ src/main/java/com/redis/Chapter1Application.java
@@ -0,0 +1,33 @@
+package com.didispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+
+@SpringBootApplication
+public class Chapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+
+				ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+				ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+
+				container.addErrorPages(error401Page, error404Page, error500Page);
+			}
+		};
+	}
+}
